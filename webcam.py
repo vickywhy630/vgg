f95447571ff33bd7afc182910d2ccc4a55974ea8
vickywhy630 <92068744+vickywhy630@users.noreply.github.com>
2023-05-19 22:34:31 -0500

    Create webcam.py

---
+++ webcam.py
@@ -0,0 +1,14 @@
+
+
+
+
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        # img = process(img)
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
