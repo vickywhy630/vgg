caf746754a4aabed7d786c291057daf98cce27ae
vickywhy630 <92068744+vickywhy630@users.noreply.github.com>
2023-05-19 22:35:19 -0500

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -13,3 +13,12 @@ class VideoProcessor:
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
+
+webrtc_ctx = webrtc_streamer(
+		key="WYH",
+		mode=WebRtcMode.SENDRECV,
+		rtc_configuration=RTC_CONFIGURATION,
+		video_processor_factory=VideoProcessor,\
+		media_stream_constraints={"video": True, "audio": False},
+		async_processing=True)
+
