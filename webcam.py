0ae050e5d52fed1be178ff5552443b6a9121168e
vickywhy630 <92068744+vickywhy630@users.noreply.github.com>
2023-05-20 20:12:27 -0500

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,12 +1,12 @@
 
 
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, VideoTransformerBase
 
-#class VideoProcessor:
-    #def recv(self, frame):
-        #img = frame.to_ndarray(format="bgr24")
+class VideoProcessor:
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
         #img = process(img)
-        #return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 #class VideoProcessor:
     #def __init__(self):
@@ -24,10 +24,9 @@ from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
         #return av.VideoFrame.from_ndarray(frame_with_bmi, format='bgr24') 
 
 
-
-RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+#RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 #webrtc_ctx = webrtc_streamer(key="WYH",mode=WebRtcMode.SENDRECV,rtc_configuration=RTC_CONFIGURATION,video_processor_factory=VideoProcessor,\media_stream_constraints={"video": True, "audio": False},async_processing=True)
 
-webrtc_streamer(key="example",rtc_configuration=RTC_CONFIGURATION,sendback_audio=False)
+webrtc_streamer(key="example",video_processor_factory=VideoProcessor,sendback_audio=False)
   
