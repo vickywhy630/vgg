70eb3cf8ed4146c2dc2ebc3321ac0d1f7203b396
vickywhy630 <92068744+vickywhy630@users.noreply.github.com>
2023-05-20 20:55:39 -0500

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -48,33 +48,32 @@ faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         
 def predict_bmi(frame):
 	pred_bmi = []
-    faces = faceCascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor = 1.15,minNeighbors = 5,minSize = (30,30))
-for (x, y, w, h) in faces:
-	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        image = frame[y:y+h, x:x+w]
-        preprocessed_img= preprocess_image(image)
-	preprocessed_img = np.expand_dims(preprocessed_img, axis=0)
-	preprocessed_img = utils.preprocess_input(preprocessed_img, version=2)
-	embeddings = custom_model.predict(preprocessed_img)
-        pred_bmi.append(embeddings[0][0])
-        cv2.putText(frame, f'BMI: {preds}', (x+5, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    return pred_bmi, frame
+	faces = faceCascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor = 1.15,minNeighbors = 5,minSize = (30,30))
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+		image = frame[y:y+h, x:x+w]
+		preprocessed_img= preprocess_image(image)
+		preprocessed_img = np.expand_dims(preprocessed_img, axis=0)
+		preprocessed_img = utils.preprocess_input(preprocessed_img, version=2)
+		embeddings = custom_model.predict(preprocessed_img)
+		pred_bmi.append(embeddings[0][0])
+		cv2.putText(frame, f'BMI: {preds}', (x+5, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+	return pred_bmi, frame
  
         
 class VideoProcessor:
-    def __init__(self):
-        self.frame_lock = threading.Lock()
-        self.out_image = None
-        self.pred_bmi = []
-
-    def recv(self, frame):
-        frm = frame.to_ndarray(format='bgr24')
-        pred_bmi, frame_with_bmi = predict_bmi(frm)
-        with self.frame_lock:
-            self.out_image = frame_with_bmi
-            self.pred_bmi = pred_bmi
-
-        return av.VideoFrame.from_ndarray(frame_with_bmi, format='bgr24') 
+	def __init__(self):
+		self.frame_lock = threading.Lock()
+		self.out_image = None
+		self.pred_bmi = []
+		
+	def recv(self, frame):
+		frm = frame.to_ndarray(format='bgr24')
+		pred_bmi, frame_with_bmi = predict_bmi(frm)
+		with self.frame_lock:
+			self.out_image = frame_with_bmi
+			self.pred_bmi = pred_bmi
+		return av.VideoFrame.from_ndarray(frame_with_bmi, format='bgr24') 
 
 
 #RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
